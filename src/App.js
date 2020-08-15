3422c533a0b5875011cdea84c4753025c129a978
vehx <delta222@gmail.com>
2020-08-15 18:04:21 +0200

    added a gamebox for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import GameBox from "./components/GameBox";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>Placeholder</p>
+                <p>Next 5 games to be released :</p>
+                <GameBox />
             </header>
         </div>
     );
