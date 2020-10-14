89798781d2fe303471bc4d554b3dd91bca8f0499
vehx <delta222@gmail.com>
2020-10-14 15:59:00 +0200

    added vue router

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -10,8 +10,8 @@
         <p v-if="loading">Loading...{{ url }}</p>
         <p v-if="error">{{ error }}</p>
         <div v-if="game">
-            <p>A game was loaded</p>
-            <p>{{ game[0].name }}</p>
+          <p>A game was loaded</p>
+          <p>{{ game[0].name }}</p>
         </div>
       </li>
     </ul>
@@ -22,56 +22,52 @@
 export default {
   name: "GameList",
   props: {
-    title: String,
+    title: String
   },
-    data () {
-      return {
-          loading: false,
-          game: null,
-          error: null,
-        //   token: process.env.VUE_APP_API_ACCESS_TOKEN
-        //   id: process.env.VUE_APP_API_CLIENT_ID
-          url: process.env.VUE_APP_API_URL
+  data() {
+    return {
+      loading: false,
+      game: null,
+      error: null,
+      //   token: process.env.VUE_APP_API_ACCESS_TOKEN
+      //   id: process.env.VUE_APP_API_CLIENT_ID
+      url: process.env.VUE_APP_API_URL
+    };
+  },
+  created() {
+    this.fetchGame();
+  },
+  methods: {
+    async fetchGame() {
+      this.loading = true;
+      const url = process.env.VUE_APP_API_URL;
+      // const clientID = process.env.VUE_APP_API_CLIENT_ID;
+      // const token = process.env.VUE_APP_API_ACCESS_TOKEN;
+      // console.log(process.env.VUE_APP_API_URL);
+      // console.log(process.env.VUE_APP_API_CLIENT_ID);
+      // console.log(process.env.VUE_APP_API_ACCESS_TOKEN);
+
+      try {
+        // const res = await fetch('https://api.kanye.rest')
+        const res = await fetch(url, {
+          method: "POST",
+          // headers: {
+          //     "Accept": "application/json",
+          //     "Client-ID": clientID,
+          //     "Authorization": "Bearer " + token,
+          // },
+          body: "fields *; limit 1;"
+        });
+        this.game = await res.json();
+        console.log(this.game);
+        console.log(this.game[0].name);
+        this.loading = false;
+      } catch (error) {
+        this.loading = false;
+        this.error = error;
       }
-    },
-    created () {
-        this.fetchGame();
-    },
-    methods: {
-        async fetchGame () {
-            this.loading = true;
-            const url = process.env.VUE_APP_API_URL;
-            // const clientID = process.env.VUE_APP_API_CLIENT_ID;
-            // const token = process.env.VUE_APP_API_ACCESS_TOKEN;
-            // console.log(process.env.VUE_APP_API_URL);
-            // console.log(process.env.VUE_APP_API_CLIENT_ID);
-            // console.log(process.env.VUE_APP_API_ACCESS_TOKEN);
-
-            try {
-                // const res = await fetch('https://api.kanye.rest')
-                const res = await fetch(
-                    url,
-                    {
-                        method: "POST",
-                        // headers: {
-                        //     "Accept": "application/json",
-                        //     "Client-ID": clientID,
-                        //     "Authorization": "Bearer " + token,
-                        // },
-                        body: "fields *; limit 1;"
-                    }
-                );
-                this.game = await res.json();
-                console.log(this.game);
-                console.log(this.game[0].name);
-                this.loading = false;
-            } catch (error) {
-                this.loading = false;
-                this.error = error;
-            }
-
-        }
     }
+  }
 };
 </script>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <GameList msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GameList from "@/components/GameList.vue";
+
+export default {
+  name: "Home",
+  components: {
+    GameList
+  }
+};
+</script>
