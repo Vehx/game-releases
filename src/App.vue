7ddeb168b253866b6776d8a66db004f72289da41
vehx <delta222@gmail.com>
2020-10-15 17:58:25 +0200

    added page fade in, not final

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <TheNavigation />
-  <router-view />
+  <transition name="fade" mode="out-in">
+    <router-view :key="$route.path" />
+  </transition>
 </template>
 
 <script>
@@ -19,5 +21,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
