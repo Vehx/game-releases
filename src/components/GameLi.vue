67b23907873f300f2a08df8f0c013a4abf0f5059
vehx <delta222@gmail.com>
2020-10-16 19:39:01 +0200

    added start of countdown loop

--- src/components/GameLi.vue
+++ src/components/GameLi.vue
@@ -3,15 +3,18 @@
     <h3>
       {{ name }}
     </h3>
-    <p v-if="isreleased">
-      {{ name }} released on:
-      {{ new Intl.DateTimeFormat().format(release * 1000) }}
+    <p>
+      {{ name }} release{{ isReleased ? "d" : "s" }} on:
+      {{ releaseDateString }}
     </p>
-    <p v-else>
-      {{ name }} releases in:
-      {{ countdown }}
-      {{ now }}
+    <p>
+      Releases in:
+      {{ currentCountdownTime }}
+      <!-- {{ formattedCountdown }} -->
     </p>
+    <p>Debug numbers:</p>
+    <p>Countdown: {{ countdown }}</p>
+    <p>Now: {{ now }}</p>
     <router-link
       :to="{
         name: 'GameDetails',
@@ -37,16 +40,48 @@ export default {
     name: String,
     release: Number
   },
+
   computed: {
     now() {
-      return new Date().getTime();
+      return Math.floor(new Date().getTime() / 1000);
     },
     countdown() {
-      return this.release * 1000 - this.now;
+      return this.release - this.now;
     },
-    isreleased() {
-      return this.countdown <= this.release;
+    isReleased() {
+      return this.countdown <= 0;
+    },
+    releaseDateString() {
+      return new Intl.DateTimeFormat().format(this.release * 1000);
     }
+    // currentCountdownTime() {
+    //   return this.countdown;
+    // }
+    // timer research link 1 https://medium.com/js-dojo/how-to-create-an-animated-countdown-timer-with-vue-89738903823f
+  },
+  data() {
+    return {
+      timeInterval: null,
+      currentCountdownTime: 100
+    };
+  },
+  methods: {
+    startCountdown() {
+      console.log(this.countdown, this.currentCountdownTime);
+      this.timeInterval = setInterval(
+        () => (this.currentCountdownTime -= 1),
+        1000
+      );
+    },
+    clearTimeInterval() {
+      clearInterval(this.timeInterval);
+    }
+  },
+  mounted() {
+    this.startCountdown();
+  },
+  beforeUnmount() {
+    this.clearTimeInterval();
   }
 };
 </script>
@@ -56,6 +91,7 @@ h3 {
   color: var(--color-green);
 }
 li {
+  max-width: 100%;
   margin: 5px 10px;
   padding: 5px;
   border: 1px solid var(--color-green);
