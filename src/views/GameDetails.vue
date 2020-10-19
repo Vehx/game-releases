40ba3e45b8d1d15c8f6b8b4d269785fc2e7a0cc4
vehx <delta222@gmail.com>
2020-10-19 17:42:54 +0200

    added proper fetching of game info

--- src/views/GameDetails.vue
+++ src/views/GameDetails.vue
@@ -1,38 +1,64 @@
 <template>
   <div>
     <h2>Game details POGGERS</h2>
-    <p>Current game id: {{ game.id }}</p>
-    <p>Current game slug: {{ game.slug }}</p>
-    <p>Current game name: {{ game.name }}</p>
-    <p>
-      Current game releases on:
-      {{ new Intl.DateTimeFormat().format(game.release * 1000) }}
+    <p v-if="loading">
+      Loading...
     </p>
-    <p>Current game summary: {{ game.summary }}</p>
+    <p v-if="error"></p>
+    <div v-if="game">
+      <p>Current game id: {{ game.id }}</p>
+      <p>Current game slug: {{ game.slug }}</p>
+      <p>Current game name: {{ game.name }}</p>
+      <p>
+        Current game releases on:
+        {{ new Intl.DateTimeFormat().format(game.first_release_date * 1000) }}
+      </p>
+      <p>Current game summary: {{ game.summary }}</p>
+    </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      loading: false,
+      game: null,
+      error: null,
+      url: process.env.VUE_APP_API_URL
+    };
+  },
   computed: {
-    game() {
-      let game = {};
-      // check if params where sent, if not fetch from api
-      if (this.$route.params.name === undefined) {
-        // do the fetching
-        // TODO
-        game.id = 123;
-        game.slug = "testname";
-        game.name = "Testname";
-        game.release = 12345;
-        game.summary = "testname summary";
-      } else {
-        game = this.$route.params;
-        // not very type safe, looking for better solution
-        game.release = parseInt(game.release);
-        game.id = parseInt(game.id);
+    slug() {
+      return this.$route.params.slug;
+    }
+  },
+  created() {
+    this.fetchGame();
+  },
+  methods: {
+    async fetchGame() {
+      this.loading = true;
+      const url = process.env.VUE_APP_API_URL;
+
+      try {
+        const res = await fetch(url, {
+          method: "POST",
+          // add headers here if needed for authentication to aws proxy
+          // headers: {
+          //     "Accept": "application/json",
+          //     "Client-ID": clientID,
+          //     "Authorization": "Bearer " + token,
+          // },
+          body: `fields *, genres.name; where slug = "${this.slug}";`
+        });
+        const result = await res.json();
+        this.game = result[0];
+        console.log(this.game);
+        this.loading = false;
+      } catch (error) {
+        this.loading = false;
+        this.error = error;
       }
-      console.log(game);
-      return game;
     }
   }
 };
