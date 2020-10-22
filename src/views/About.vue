80390a9d3edb9641601ef34699df225278e99005
vehx <delta222@gmail.com>
2020-10-22 21:09:11 +0200

    added some info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      About <br />
+      Game Releases
+    </h1>
+    <h3>This site is under construction :)</h3>
+    <p class="tiny">please excuse the noise</p>
+    <p>
+      When it's done it will show game releases of the coming week.
+    </p>
+    <p>
+      There will also be a search so you can find a game you're looking forward
+      to.
+    </p>
+    <p>
+      And a watchlist that you can add games to and keep track of. (Only saved
+      locally)
+    </p>
   </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 0 1.5rem;
+}
+h1 {
+  margin: 2rem;
+}
+.tiny {
+  font-size: 10px;
+}
+p {
+  margin-bottom: 1rem;
+}
+</style>
