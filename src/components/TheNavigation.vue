da07056d27d23b55148803eb567e87b6c14c8a69
vehx <delta222@gmail.com>
2020-11-02 14:28:35 +0100

    added watcher for menu closing on navigation

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,6 +1,5 @@
 <template>
   <nav
-    ref="menu"
     id="nav"
     :class="[{ open: isMenuOpen, menu: !isMobile, hamburger: isMobile }]"
   >
@@ -9,6 +8,8 @@
     <router-link to="/playstation">Playstation</router-link>
     <router-link to="/xbox">Xbox</router-link>
     <router-link to="/switch">Switch</router-link>
+    <router-link to="/search">Search</router-link>
+    <router-link to="/watch-list">Watch list</router-link>
     <router-link to="/about">About</router-link>
   </nav>
   <div class="buttons">
@@ -64,8 +65,14 @@ export default {
     },
     checkWindowWidth() {
       this.isMobile = window.innerWidth <= this.mobileMaxWidth;
-      console.log(this.isMobile);
+      // console.log(this.isMobile);
       // console.log(window.innerWidth);
+    }
+  },
+  watch: {
+    $route() {
+      document.documentElement.style.overflow = "auto";
+      this.isMenuOpen = false;
     }
   }
 };
@@ -90,8 +97,8 @@ a.active-link {
 .hamburger {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
+  justify-content: space-evenly;
+  align-items: center;
   height: 100%;
   width: 0;
   position: fixed;
@@ -100,7 +107,7 @@ a.active-link {
   right: 0;
   background-color: var(--color-background-faded);
   overflow-x: hidden;
-  padding-top: 4rem;
+  /* padding-top: 4rem; */
   transition: 0.5s;
 }
 .button-menu {
