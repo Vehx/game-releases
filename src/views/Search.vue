6232a25a56c358ad2c8ae7b87621d35f922477a2
vehx <delta222@gmail.com>
2020-11-03 17:52:50 +0100

    added basic search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,17 +2,30 @@
   <div class="search">
     <h1>Search page</h1>
     <h2>Under construction</h2>
-    <!-- <GameList title="Search results: " /> -->
+    <label for="search">Search</label>
+    <input type="text" v-model.lazy="searchTerm" name="search" id="search" />
+    <p>Search term: {{ searchTerm }}</p>
+    <GameList
+      v-if="searchTerm"
+      title="Search results: "
+      search
+      :searchTerm="searchTerm"
+    />
   </div>
 </template>
 
 <script>
-// import GameList from "@/components/GameList.vue";
+import GameList from "@/components/GameList.vue";
 
 export default {
   name: "Search",
   components: {
-    // GameList
+    GameList
+  },
+  data() {
+    return {
+      searchTerm: ""
+    };
   }
 };
 
